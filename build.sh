3aaa27ddee8172cb0cc364fdd17bda1db278c74f
yzfzzz <18320300436@163.com>
2024-10-25 19:39:07 +0800

    🐳 chore: 修改编译过程

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
-# #!/bin/bash
-# cd ..
-
-# if [ -d "build" ]; then
-#     echo "Build directory already exists."
-#     rm -r build
-# else
-#     echo "Created build directory."
-# fi
-
-# mkdir build
-# cd build
-# cmake ../work
-# make -j2
-
 #!/bin/bash
+cd ..
 
 if [ -d "build" ]; then
     echo "Build directory already exists."
@@ -24,5 +10,5 @@ fi
 
 mkdir build
 cd build
-cmake ..
+cmake ../work
 make -j2
