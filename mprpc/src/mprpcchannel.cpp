1bfc7241feb5cb8baf5102ac6aa064ccd84763f1
yzfzzz <20zfye@stu.edu.cn>
2024-11-03 18:54:10 +0800

    🎈 perf: 优化终端信息输出

--- mprpc/src/mprpcchannel.cpp
+++ mprpc/src/mprpcchannel.cpp
@@ -56,12 +56,12 @@ void MprpcChannel::CallMethod(const google::protobuf::MethodDescriptor* method,
     send_rpc_str += args_str;
 
     // 打印调试信息
-    std::cout << "=========================" << std::endl;
-    std::cout << "header_size: " << header_size << std::endl;
-    std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
-    std::cout << "service_name: " << service_name << std::endl;
-    std::cout << "method_name: " << method_name << std::endl;
-    std::cout << "args_str: " << args_str << std::endl;
+    // std::cout << "=========================" << std::endl;
+    // std::cout << "header_size: " << header_size << std::endl;
+    // std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
+    // std::cout << "service_name: " << service_name << std::endl;
+    // std::cout << "method_name: " << method_name << std::endl;
+    // std::cout << "args_str: " << args_str << std::endl;
 
     // 使用tcp编程
     int clientfd = socket(AF_INET, SOCK_STREAM, 0);
--- mprpc/src/rpcprovider.cpp
+++ mprpc/src/rpcprovider.cpp
@@ -128,12 +128,12 @@ void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr& conn, muduo::net
     std::string args_str = recv_buf.substr(4 + header_size, args_size);
 
     // 打印调试信息
-    std::cout << "=========================" << std::endl;
-    std::cout << "header_size: " << header_size << std::endl;
-    std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
-    std::cout << "service_name: " << service_name << std::endl;
-    std::cout << "method_name: " << method_name << std::endl;
-    std::cout << "args_str: " << args_str << std::endl;
+    // std::cout << "=========================" << std::endl;
+    // std::cout << "header_size: " << header_size << std::endl;
+    // std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
+    // std::cout << "service_name: " << service_name << std::endl;
+    // std::cout << "method_name: " << method_name << std::endl;
+    // std::cout << "args_str: " << args_str << std::endl;
 
     // 获取service对象和method对象
     auto it = m_serviceMap.find(service_name);
