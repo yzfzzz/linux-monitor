bb3a013d7fffee171b826ce65362f277ae8e3b3d
yzfzzz <20zfye@stu.edu.cn>
2024-11-03 18:58:06 +0800

    🎈 perf: 优化运行逻辑

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,15 @@
-rm -r py_cpp_pipe.fifo
-./monitor -i address.conf & python write.py
+if [ -d "logs" ]; then
+    echo "logs directory already exists."
+    rm -r logs
+else
+    echo "Created logs directory."
+fi
+
+mkdir logs
+
+if [ -d "py_cpp_pipe.fifo" ]; then
+    echo "Build directory already exists."
+    rm -r py_cpp_pipe.fifo
+fi
+
+python3 write.py & ./monitor -i address.conf
