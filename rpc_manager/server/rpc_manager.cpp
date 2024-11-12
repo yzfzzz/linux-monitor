a88f2fb6215552047ed70d12898b1548c5773902
yzfzzz <20zfye@stu.edu.cn>
2024-11-12 09:40:50 +0800

    ✨ feat: 新增服务端向数据库插入数据功能
    该功能还处于测试阶段

--- rpc_manager/server/rpc_manager.cpp
+++ rpc_manager/server/rpc_manager.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <mutex>
+#include "json.hpp"
 #include "rpc_manager.h"
 using namespace std;
-#include <mutex>
 mutex mtx;
 namespace monitor {
 
@@ -19,14 +20,14 @@ ServerManagerImpl::~ServerManagerImpl() {}
 //     return grpc::Status::OK;
 // }
 
-void ServerManagerImpl::SetMonitorInfo(::google::protobuf::RpcController* controller,
-                                       const ::monitor::proto::MonitorInfo* request,
-                                       ::google::protobuf::Empty* response,
-                                       ::google::protobuf::Closure* done) {
+void ServerManagerImpl::SetMonitorInfo(
+    ::google::protobuf::RpcController* controller,
+    const ::monitor::proto::MonitorInfo* request,
+    ::google::protobuf::Empty* response, ::google::protobuf::Closure* done) {
     mtx.lock();
     monitor_infos_.Clear();
     monitor_infos_ = *request;
-
+    InsertOneInfo(monitor_infos_);
     cout << "jinru" << request->soft_irq_size() << endl;
     done->Run();
     mtx.unlock();
@@ -39,14 +40,65 @@ void ServerManagerImpl::SetMonitorInfo(::google::protobuf::RpcController* contro
 //     return grpc::Status::OK;
 // }
 
-void ServerManagerImpl::GetMonitorInfo(::google::protobuf::RpcController* controller,
-                                       const ::google::protobuf::Empty* request,
-                                       ::monitor::proto::MonitorInfo* response,
-                                       ::google::protobuf::Closure* done) {
+void ServerManagerImpl::GetMonitorInfo(
+    ::google::protobuf::RpcController* controller,
+    const ::google::protobuf::Empty* request,
+    ::monitor::proto::MonitorInfo* response,
+    ::google::protobuf::Closure* done) {
     mtx.lock();
     *response = monitor_infos_;
     done->Run();
     mtx.unlock();
+}
+
+ServerManagerImpl::MidInfo ServerManagerImpl::parseInfos(monitor::proto::MonitorInfo& monitor_infos_) {
+    MidInfo mifo;
+    minfo.gpu_num = monitor_info.gpu_info_size();
+    minfo.gpu_name = monitor_info.gpu_info(0).gpu_name();
+    minfo.gpu_used_mem = monitor_info.gpu_info(0).gpu_mem_used();
+    minfo.gpu_total_mem = monitor_info.gpu_info(0).gpu_mem_total();
+    minfo.gpu_avg_util = monitor_info.gpu_info(0).gpu_mem_utilize();
+    minfo.cpu_load_avg_1 = monitor_info.cpu_load().load_avg_1();
+    minfo.cpu_load_avg_3 = monitor_info.cpu_load().load_avg_3();
+    minfo.cpu_load_avg_15 = monitor_info.cpu_load().load_avg_15();
+    minfo.mem_used = monitor_info.mem_info().used_percent();
+    minfo.mem_total = monitor_info.mem_info.total();
+    minfo.net_send_rate = monitor_info.net_info(0).send_rate();
+    minfo.net_rcv_rate = monitor_info.net_info(0).rcv_rate();
+
+    minfo.ip_addr = monitor_info.ip_addr();
+    return mifo;
+}
+
+bool ServerManagerImpl::InsertOneInfo(
+    monitor::proto::MonitorInfo& monitor_infos_) {
+    std::shared_ptr<MysqlConn> conn_ptr = this->pool->getConnection();
+    std::string sql = "INSERT INTO table_20241111
+                    (gpu_name, gpu_num, gpu_used_mem, gpu_avg_util, cpu_load_avg_1, cpu_load_avg_3, cpu_load_avg_15, mem_used, net_send_rate, net_rcv_rate, user_id)
+                    VALUES(";
+    MidInfo mifo = parseInfos(monitor_infos_);
+    int user_id = SelectUserId(mifo.ip_addr);
+    if (user_id == -1) {
+        return false;
+    }
+    sql = sql+mifo.gpu_name+","+std::to_string(mifo.gpu_num)+","+std::to_string(mifo.gpu_used_mem)+","+
+            std::to_string(mifo.gpu_avg_util)+","+std::to_string(mifo.cpu_load_avg_1)+","+std::to_string(mifo.cpu_load_avg_3)+","+
+            std::to_string(mifo.cpu_load_avg_15)+","+std::to_string(mifo.mem_used)+","+std::to_string(mifo.net_send_rate)+","+
+            std::to_string(mifo.net_rcv_rate)+","+std::to_string(user_id)+")";
+    return conn_ptr->update(sql);
+}
+
+int ServerManagerImpl::SelectUserId(std::string ip) {
+    std::shared_ptr<MysqlConn> conn_ptr = this->pool->getConnection();
+    std::string sql = "SELECT id FROM `user` u WHERE ip_addr " + ip;
+    int user_id;
+    if (conn_ptr->update(sql) == true) {
+        while (connptr->next()) {
+            user_id = connptr->value(0);
+        }
+        return user_id;
+    }
+    return -1;
 }
 
 }  // namespace monitor
--- rpc_manager/server/rpc_manager.h
+++ rpc_manager/server/rpc_manager.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <memory>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include "ConnectionPool.h"
+#include "MysqlConn.h"
+#include "monitor_info.pb.h"
 #include "mprpcapplication.h"
 #include "rpcprovider.h"
-#include <unordered_map>
-#include "monitor_info.pb.h"
 
 namespace monitor {
 class ServerManagerImpl : public monitor::proto::MonitorManager {
@@ -11,16 +16,39 @@ class ServerManagerImpl : public monitor::proto::MonitorManager {
     virtual ~ServerManagerImpl();
 
     void SetMonitorInfo(::google::protobuf::RpcController* controller,
-                       const ::monitor::proto::MonitorInfo* request,
-                       ::google::protobuf::Empty* response,
-                       ::google::protobuf::Closure* done);
+                        const ::monitor::proto::MonitorInfo* request,
+                        ::google::protobuf::Empty* response,
+                        ::google::protobuf::Closure* done);
 
     void GetMonitorInfo(::google::protobuf::RpcController* controller,
-                       const ::google::protobuf::Empty* request,
-                       ::monitor::proto::MonitorInfo* response,
-                       ::google::protobuf::Closure* done);
+                        const ::google::protobuf::Empty* request,
+                        ::monitor::proto::MonitorInfo* response,
+                        ::google::protobuf::Closure* done);
+
+    void InsertOneInfo();
+    int SelectUserId();
+
+    struct MidInfo {
+        int gpu_num;
+        string gpu_name;
+        int gpu_used_mem;
+        int gpu_total_mem;
+        int gpu_avg_util;
+        float cpu_load_avg_1;
+        float cpu_load_avg_3;
+        float cpu_load_avg_15;
+        float mem_used;
+        float mem_total;
+        float net_send_rate;
+        float net_rcv_rate;
+
+        string ip_addr
+    };
+
+    MidInfo parseInfos(monitor::proto::MonitorInfo request);
 
    private:
     monitor::proto::MonitorInfo monitor_infos_;
+    ConnectionPool* pool = ConnectionPool::getConnectPool();
 };
 }  // namespace monitor
