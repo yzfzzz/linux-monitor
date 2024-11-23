21da7a71fff2caacf6a332c601f7290291a493d3
yzfzzz <20zfye@stu.edu.cn>
2024-11-23 19:58:15 +0800

    🐞 fix: 修复了客户端曲线重叠的问题

--- display_monitor/main.cpp
+++ display_monitor/main.cpp
@@ -5,29 +5,32 @@
 #include <vector>
 #include "query_data.h"
 #include "widget.h"
-#include <mutex>
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Widget w;
     w.show();
     std::unique_ptr<std::thread> thread_;
-    bool flag = false;
     thread_ = std::make_unique<std::thread>([&]() {
+        monitor::queryData queryData;
+        if (queryData.queryDataInfo("12345678", 30)) {
+            for (int i = 0; i < 30; i++) {
+                w.Update(queryData.queryData_array[i]);
+
+                // TODO: 为什么加上这一句就可以了？
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        }
         while (true) {
             int n = 1;
-            if(!flag)
-            {
-                n = 30;
-                flag = true;
-            } 
-            monitor::queryData queryData;
+            queryData.queryData_array.clear();
             if (queryData.queryDataInfo("12345678", n)) {
                 for (int i = 0; i < n; i++) {
                     w.Update(queryData.queryData_array[i]);
                 }
             }
-            std::this_thread::sleep_for(std::chrono::seconds(3));
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     });
     thread_->detach();
